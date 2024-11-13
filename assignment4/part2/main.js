3c89aeee11ee9b0f7c7db1be6e1871b8896f39d0
JustinB514 <justin.bourne1@dcmail.ca>
2024-11-13 10:54:12 -0500

    Updated part 2 of assignment 4

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -5,14 +5,23 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imgArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
-/* Declaring the alternative text for each image file */
+/* TO DO: Declaring the alternative text for each image file */
+const altText = ['A closeup of an eye', '', 'Some pretty flowers', 'Ancient Egyptian artwork', 'A butterfly resting on a leaf']
 
 /* Looping through images */
+for (let i = 0; i < imgArray.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/' + imgArray[i]);
+    newImage.setAttribute('alt', '');
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(e) {
+        displayedImage.src = e.target.src
+        //TO DO: altText for each displayedImage
+    }
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
+// TO DO:
 /* Wiring up the Darken/Lighten button */
