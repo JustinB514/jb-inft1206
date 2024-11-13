0c9419e69f4ee0780953f0f52531044309d80ea1
JustinB514 <justin.bourne1@dcmail.ca>
2024-11-13 13:37:42 -0500

    Updated part 2 of assignment 4

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -7,21 +7,25 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 const imgArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
-/* TO DO: Declaring the alternative text for each image file */
-const altText = ['A closeup of an eye', '', 'Some pretty flowers', 'Ancient Egyptian artwork', 'A butterfly resting on a leaf']
+/*Declaring the alternative text for each image file */
+const altText = ['A close-up of an eye', 'A random image', 'Some pretty flowers', 'Ancient Egyptian artwork', 'A butterfly resting on a leaf']
 
 /* Looping through images */
 for (let i = 0; i < imgArray.length; i++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', 'images/' + imgArray[i]);
-    newImage.setAttribute('alt', '');
+    newImage.setAttribute('alt', altText[i]);
     thumbBar.appendChild(newImage);
     newImage.onclick = function(e) {
         displayedImage.src = e.target.src
-        //TO DO: altText for each displayedImage
+        displayedImage.altText = e.target.altText
     }
 }
 
-
 // TO DO:
 /* Wiring up the Darken/Lighten button */
+//btn.onclick = 
+btn.setAttribute("class", xxx);
+btn.textContent = xxx;
+overlay.computedStyleMap.backgroundColor = xxx;
+
